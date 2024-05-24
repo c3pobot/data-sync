129b451068921961fabb920ce50055e5aab764ed
scuba75 <scuba75@gmail.com>
2024-05-24 07:02:42 -0500

    update

--- src/updateAutoComplete/maps/journey.js
+++ src/updateAutoComplete/maps/journey.js
@@ -2,9 +2,20 @@
 const mongo = require('mongoclient')
 module.exports = async(gameVersion, localeVersion)=>{
   let obj = await mongo.find('journeyGuide', {}, { baseId: 1, nameKey: 1, unitNameKey: 1})
-  let autoComplete = obj?.map(x=>{
-    return { name: x.unitNameKey, value: x.baseId, descKey: x.nameKey }
-  })
+  let manualGuides = (mongo.find('botSettings', { _id: 'manualGuides' }))[0]
+  let autoComplete = [], tempSet = new Set()
+  for(let i in obj){
+    if(tempSet.has(obj[i].baseId)) continue
+    autoComplete.push({ name: obj[i].unitNameKey, value: obj[i].baseId, descKey: obj[i].nameKey })
+    tempSet.add(obj[i].baseId)
+  }
+  if(manualGuides?.data?.length > 0){
+    for(let i in manualGuides?.data){
+      if(tempSet.has(manualGuides.data[i].value)) continue
+      autoComplete.push(manualGuides.data[i])
+      tempSet.add(manualGuides.data[i].value)
+    }
+  }
   if(autoComplete?.length > 0){
     await mongo.set('autoComplete', { _id: 'journey' }, { include: true, data: autoComplete, gameVersion: gameVersion, localeVersion: localeVersion })
     return true
