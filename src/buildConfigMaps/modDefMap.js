9edcab86f9ece3325e88d03e1da673758399f8b1
scuba75 <scuba75@gmail.com>
2024-06-25 10:37:54 -0500

    update

--- src/buildConfigMaps/modDefMap.js
+++ src/buildConfigMaps/modDefMap.js
@@ -3,10 +3,22 @@ const mongo = require('mongoclient')
 const getFile = require('src/helpers/getFile')
 
 module.exports = async(gameVersion, localeVersion)=>{
-  let statModList = await getFile('statMod', gameVersion)
-  if(!statModList) return
-  let data = {}
-  for(let i in statModList) data[statModList[i].id] = { defId: statModList[i].id, rarity: statModList[i].rarity, slot: +statModList[i].slot, setId: +statModList[i].setId }
+  let [ statModList, statModSetList, lang ] = await Promise.all([
+    getFile('statMod', gameVersion),
+    getFile('statModSet', gameVersion),
+    getFile('Loc_ENG_US.txt', localeVersion)
+  ])
+  if(!statModList || !statModSetList || !lang) return
+  let data = {}, setList = {}
+  for(let i in statModList){
+    let modSet = setList[statModList[i].setId]
+    if(!modSet){
+      modSet = statModSetList.find(x=>x.id === statModList[i].setId)
+      if(modSet) setList[statModList[i].setId] = modSet
+    }
+    if(!modSet) continue
+    data[statModList[i].id] = { defId: statModList[i].id, rarity: statModList[i].rarity, slot: +statModList[i].slot, setId: +statModList[i].setId, nameKey: lang[modSet.name] || modSet.name, setCount: modSet.setCount, icon: modSet.icon }
+  }
   await mongo.set('configMaps', { _id: 'modDefMap' }, { gameVersion: gameVersion, localeVersion: localeVersion, data: data })
   return true
 }
--- src/updateAutoComplete/maps/ga-date.js
+++ src/updateAutoComplete/maps/ga-date.js
@@ -6,6 +6,7 @@ module.exports = async(gameVersion, localeVersion)=>{
   obj = obj?.filter(x=>x.leaderboardScanComplete)
   if(!obj || obj?.length == 0) return
   let autoComplete = obj.filter(x=>x.leaderboardScanComplete).map(x=>{ return { name: `${x.date} (${x.mode})`, value: x.key, mode: x.mode, eventInstanceId: x.id } })
+  console.log(autoComplete?.length)
   autoComplete = sorter([{ column:  'value', order: 'descending' }], autoComplete || [])
   if(autoComplete?.length > 0) await mongo.set('autoComplete', { _id: 'ga-date' }, { data: autoComplete, include: true, gameVersion: gameVersion, localeVersion: localeVersion })
   return true
