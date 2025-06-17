29309a0e5527f309af72d3a437c9154651b2b60d
scuba75 <scuba75@gmail.com>
2025-06-17 10:57:19 -0500

    update

--- src/updateAutoComplete/maps/faction.js
+++ src/updateAutoComplete/maps/faction.js
@@ -3,7 +3,6 @@ const mongo = require('mongoclient')
 module.exports = async(gameVersion, localeVersion)=>{
   let obj = await mongo.find('factions', {}, { baseId: 1, nameKey: 1, hidden: 1 })
   let autoComplete = obj?.filter(x=>!x.hidden)?.map(x=>{
-    if(x.baseId?.startsWith('raid_')) console.log(x.baseId)
     return { name: x.nameKey, value: x.baseId }
   })
   if(autoComplete.length > 0){
